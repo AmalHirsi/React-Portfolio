1638345c07ce8fb39cce14bcb06454747b3dc430
Amal Hirsi <ahirsi@hotmail.co.uk>
2022-12-07 09:27:32 +0000

    components set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+// import Navigation from './components/Navigation';
+import Navbar from "./components/Navbar/Navbar";
+import About from "./components/About/About";
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="app">
+      <Navbar></Navbar>
+      <div className="sections">
+        <About></About>
+        <Projects></Projects>
+        <Contact></Contact>
+        <Footer></Footer>  
+      </div>
+      <div className="footer">
+      <Footer></Footer>  
+      </div>
+      </div>  
+  )
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import "./Navbar.css";
+
+function Navbar() {
+
+    return (
+        <div className="topbar">
+                <div className="right">
+                    <ul>
+                        <li><a href="#about">About Me</a></li>
+                        <li><a href="#portfolio">Projects</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+
+            </div>
+    );
+}
+export default Navbar;
+
+
